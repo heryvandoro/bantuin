ad4e6d5af1bbf99b511a001507aa67a357f82122
cathelines <gabriellyan.sanger@binus.edu>
2017-11-22 12:20:58 +0700

    added styles

--- app/src/main/java/net/slc/bantuin/UIActivity.java
+++ app/src/main/java/net/slc/bantuin/UIActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -15,13 +16,14 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
-//panda a
+
 public class UIActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_ui);
 
         List<AuthUI.IdpConfig> providers = Arrays.asList(
