24a15e0eb888196810bac4861da790eb4d705cfb
Hery Vandoro <vandorohery99@gmail.com>
2017-11-24 04:17:12 +0700

    Add off-focus listener and auto hide keyboard if out of focus

--- app/src/main/java/net/slc/bantuin/RegisterActivity.java
+++ app/src/main/java/net/slc/bantuin/RegisterActivity.java
@@ -1,10 +1,15 @@
 package net.slc.bantuin;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -87,5 +92,22 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 doRegister();
                 break;
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof TextInputLayout || v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 }
