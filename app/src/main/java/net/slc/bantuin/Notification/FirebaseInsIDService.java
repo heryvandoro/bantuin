9232c782085e840ba4b4f09178d54485bfb0e2ec
Hery Vandoro <vandorohery99@gmail.com>
2017-11-25 11:55:41 +0700

    Integrated with simple FCM

---
+++ app/src/main/java/net/slc/bantuin/Notification/FirebaseInsIDService.java
@@ -0,0 +1,18 @@
+package net.slc.bantuin.Notification;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInsIDService extends FirebaseInstanceIdService{
+    @Override
+    public void onTokenRefresh() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+        //Log .d("token", "Refreshed token: " + token);
+
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
---
+++ app/src/main/java/net/slc/bantuin/Notification/FirebaseMsgService.java
@@ -0,0 +1,29 @@
+package net.slc.bantuin.Notification;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMsgService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("from : ", "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d("messages : ", "Message data payload: " + remoteMessage.getData());
+            /*if ( Check if data needs to be processed by long running job  true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }*/
+        }
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d("messages body : ", remoteMessage.getNotification().getBody());
+        }
+    }
+}
