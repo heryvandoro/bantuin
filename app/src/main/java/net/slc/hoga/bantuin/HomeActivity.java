746e67410fc31b0403a408c244180e4685dd81b9
Hery Vandoro <vandorohery99@gmail.com>
2017-11-27 00:27:38 +0700

    Add icon to tab layout

--- app/src/main/java/net/slc/hoga/bantuin/HomeActivity.java
+++ app/src/main/java/net/slc/hoga/bantuin/HomeActivity.java
@@ -3,6 +3,10 @@ package net.slc.hoga.bantuin;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import net.slc.hoga.bantuin.Adapter.ViewPagerAdapter;
 import net.slc.hoga.bantuin.Fragment.AccountFragment;
@@ -13,6 +17,15 @@ import net.slc.hoga.bantuin.Fragment.HomeFragment;
 public class HomeActivity extends MasterActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
+
+    private int[] tabIcons = {
+            R.drawable.icon_home,
+            R.drawable.icon_discover,
+            R.drawable.icon_bookmark,
+            R.drawable.icon_gears
+    };
+
+    private String[] tabTitles = {"HOME", "DISCOVER", "MY EVENTS", "ACCOUNT"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +41,26 @@ public class HomeActivity extends MasterActivity {
 
         tabLayout = findViewById(R.id.tab);
         tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            LinearLayout temp = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+            TextView a =  temp.findViewById(R.id.tabText);
+            a.setText(tabTitles[i]);
+            ImageView b = temp.findViewById(R.id.tabIcon);
+            b.setBackgroundResource(tabIcons[i]);
+            tabLayout.getTabAt(i).setCustomView(temp);
+        }
     }
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new HomeFragment(), "Home");
-        adapter.addFragment(new DiscoverFragment(), "Discover");
-        adapter.addFragment(new EventFragment(), "My Events");
-        adapter.addFragment(new AccountFragment(), "Account");
+        adapter.addFragment(new HomeFragment(), tabTitles[0]);
+        adapter.addFragment(new DiscoverFragment(), tabTitles[1]);
+        adapter.addFragment(new EventFragment(), tabTitles[2]);
+        adapter.addFragment(new AccountFragment(), tabTitles[3]);
         viewPager.setAdapter(adapter);
     }
 }
