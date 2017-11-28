dc96171b6f96e6b7e3e4c88a8f1fbe593ecba631
Hery Vandoro <vandorohery99@gmail.com>
2017-11-29 01:10:19 +0700

    Fix missing content in list view (viewpager) with change getFragmentManager to getChildFragmentManager

--- app/src/main/java/net/slc/hoga/bantuin/Fragment/EventFragment.java
+++ app/src/main/java/net/slc/hoga/bantuin/Fragment/EventFragment.java
@@ -8,13 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import net.slc.hoga.bantuin.Adapter.TabAdapter;
 import net.slc.hoga.bantuin.Helper.Config;
@@ -24,12 +21,10 @@ import net.slc.hoga.bantuin.Model.Event;
 import net.slc.hoga.bantuin.Model.User;
 import net.slc.hoga.bantuin.R;
 
-import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class EventFragment extends Fragment {
@@ -59,7 +54,7 @@ public class EventFragment extends Fragment {
         partUpcoming = new EventPartFragment();
         partHistory = new EventPartFragment();
 
-        TabAdapter adapter = new TabAdapter(getFragmentManager());
+        TabAdapter adapter = new TabAdapter(getChildFragmentManager());
         adapter.addFragment(partMyevents, "My Events");
         adapter.addFragment(partUpcoming, "Upcoming");
         adapter.addFragment(partHistory, "History");
