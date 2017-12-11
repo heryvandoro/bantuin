e557d9cee77e28ab29e37ebcf76dfb90117d6ed2
Hery Vandoro <vandorohery99@gmail.com>
2017-12-11 10:14:12 +0700

    Update TEMP_PHOTO URL

--- app/src/main/java/net/slc/hoga/bantuin/Helper/Config.java
+++ app/src/main/java/net/slc/hoga/bantuin/Helper/Config.java
@@ -1,7 +1,7 @@
 package net.slc.hoga.bantuin.Helper;
 
 public class Config {
-    public static String TEMP_PHOTO = "https://www.business-software.com/wp-content/uploads/2013/02/avatar_placeholder.png";
+    public static String TEMP_PHOTO = "https://bantuin.mahirkoding.com/images/avatar_placeholder.png";
     public static String DATE_FORMAT = "dd/MM/yyyy";
     public static String BASE_URL_PICTURE = "http://bantuin.mahirkoding.com/";
     public static String BASE_URL_TOKEN = "https://bantuin-hoga-heryvandoro795356.codeanyapp.com/";
